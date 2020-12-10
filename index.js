b40348c34a1f8c5a759db7bdfbf52158aa415b9f
raysoju <raymond.lim672@gmail.com>
2020-12-10 16:23:22 +0100

    test vanilla index.js nodejs

--- index.js
+++ index.js
@@ -14,25 +14,29 @@ const webpush = require('web-push');
 const port = process.env.PORT || 8080;
 const db = process.env.DATABASE_URL || 'mongodb://localhost:27017/unptitfive';
 
-mongoose.connect(db, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-});
+// mongoose.connect(db, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useCreateIndex: true,
+// });
 
-app.use(bodyparser.urlencoded({ extended: false }));
+// app.use(bodyparser.urlencoded({ extended: false }));
+app.use(cors());
 app.use(bodyparser.json());
-app.use(cookieParser());
-app.use(
-  cors({
-    credentials: true,
-    origin: [
-      'http://localhost:3000',
-      'http://localhost:8080',
-      'https://epi-pwa-front.herokuapp.com/',
-    ],
-  }),
-);
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+// app.use(cookieParser());
+// app.use(
+//   cors({
+//     credentials: true,
+//     origin: [
+//       'http://localhost:3000',
+//       'http://localhost:8080',
+//       'https://epi-pwa-front.herokuapp.com/',
+//     ],
+//   }),
+// );
 
 webpush.setVapidDetails(
   'mailto: corentin@outlook.com',
@@ -43,12 +47,10 @@ webpush.setVapidDetails(
 app.post('/notifications/subscribe', (req, res) => {
   const subscription = req.body;
 
-  console.log(subscription);
+  const payload =
+    '{ "notification": {"title": "Hello World", "body: "un evenement", "data": {"url": "http://www.google.com"}} }';
 
-  const payload = JSON.stringify({
-    title: 'Hello!',
-    body: 'It works.',
-  });
+  console.log(subscription);
 
   webpush
     .sendNotification(subscription, payload)
@@ -57,18 +59,21 @@ app.post('/notifications/subscribe', (req, res) => {
 
   res.status(200).json({ success: true });
 });
-routes.routes.forEach((r) => {
-  app.use(r.name, r.router);
-});
 
-// Handle Error 404
-app.use((req, res) => {
-  const lang = req.acceptsLanguages();
-  res.status(404).json({ message: translate('ERROR_404', lang) });
-});
+app.listen(port);
 
-startSocket(io);
+// routes.routes.forEach((r) => {
+//   app.use(r.name, r.router);
+// });
 
-server.listen(port, () => {
-  console.log(`Server running at PORT = ${port}`);
-});
+// // Handle Error 404
+// app.use((req, res) => {
+//   const lang = req.acceptsLanguages();
+//   res.status(404).json({ message: translate('ERROR_404', lang) });
+// });
+
+// startSocket(io);
+
+// server.listen(port, () => {
+//   console.log(`Server running at PORT = ${port}`);
+// });
